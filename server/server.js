8f0f1aa69ed4c70d354a86c7c2c64a020ef7060f
hilasmirnov <bugiza@gmail.com>
2017-02-09 23:10:13 +0200

    greet new user and alert others

--- server/server.js
+++ server/server.js
@@ -14,13 +14,34 @@ app.use(express.static(publicPath));
 io.on('connection', (socket) => {
     console.log('new user connected');
 
+    // sends to the connected user
+    socket.emit('newMessage', {
+        from: 'admin',
+        text: 'Welcome!',
+        createdAt: new Date().getTime()
+    });
+
+    // sends to everybody BUT the connected user
+    socket.broadcast.emit('newMessage', {
+        from: 'Admin',
+        text: 'New user joined',
+        createdAt: new Date().getTime()
+    });
+
     socket.on('createMessage', (message) => {
         console.log('createMessage', message);
+
+        // sends to everybody
         io.emit('newMessage', {
             from: message.from,
             text: message.text,
             createdAt: new Date().getTime()
         });
+        // socket.broadcast.emit('newMessage', {
+        //     from: message.from,
+        //     text: message.text,
+        //     createdAt: new Date().getTime()
+        // });
     });
 
     socket.on('disconnect', () => {
