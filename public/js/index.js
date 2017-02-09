bc45be37b371b08660ced72e4fcbde14275d2f8c
hilasmirnov <bugiza@gmail.com>
2017-02-09 22:57:19 +0200

    emit new message on create mesage

--- public/js/index.js
+++ public/js/index.js
@@ -1,11 +1,6 @@
 var socket = io();
 socket.on('connect', function() {
     console.log('connected to server');
-
-    socket.emit('createMessage', {
-        from: 'Hila',
-        text: 'test'
-    })
 });
 socket.on('disconnect', function() {
     console.log('disconnected from server');
