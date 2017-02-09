c9d5935d49c4ab0122f2070f3a291f2f99dcb60e
hilasmirnov <bugiza@gmail.com>
2017-02-09 22:51:01 +0200

    add new message and create message events

---
+++ public/js/index.js
@@ -0,0 +1,15 @@
+var socket = io();
+socket.on('connect', function() {
+    console.log('connected to server');
+
+    socket.emit('createMessage', {
+        from: 'Hila',
+        text: 'test'
+    })
+});
+socket.on('disconnect', function() {
+    console.log('disconnected from server');
+});
+socket.on('newMessage', function(message) {
+    console.log('new message', message);
+});
