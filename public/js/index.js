16041204762f6dfb0bd2bcbed760e75d73b62a43
hilasmirnov <bugiza@gmail.com>
2017-02-10 16:34:46 +0200

    scroll to bottom if user is close to bottom

--- public/js/index.js
+++ public/js/index.js
@@ -1,4 +1,22 @@
 var socket = io();
+
+function scrollToBottom() {
+    // selectors
+    var messages = jQuery('#messages');
+    var newMessage = messages.children('li:last-child');
+    //heights
+    var clientHeight = messages.prop('clientHeight');
+    var scrollTop = messages.prop('scrollTop');
+    var scrollHeight = messages.prop('scrollHeight');
+    var newMessageHeight = newMessage.innerHeight();
+    var lastMessageHeight = newMessage.prev().innerHeight();
+
+    // only scroll if we're at the bottom of the screen (not if the user scrolled up)
+    if (clientHeight + scrollTop + newMessageHeight + lastMessageHeight >= scrollHeight) {
+        messages.scrollTop(scrollHeight);
+    }
+}
+
 socket.on('connect', function() {
     console.log('connected to server');
 });
@@ -15,6 +33,7 @@ socket.on('newMessage', function(message) {
         createdAt: formattedTime
     });
     jQuery('#messages').append(html);
+    scrollToBottom();
     // var li = jQuery('<li></li>');
     // li.text(`${message.from} ${formattedTime}: ${message.text}`);
     // jQuery('#messages').append(li);
@@ -29,7 +48,7 @@ socket.on('newLocationMessage', function(message) {
         createdAt: formattedTime
     });
     jQuery('#messages').append(html);
-
+    scrollToBottom();
 
     // var formattedTime = moment(message.createdAt).format('h:mm a');
 
